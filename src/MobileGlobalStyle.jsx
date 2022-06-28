d881f181139036fe37b35649879d4060c1e1d3bb
RootZero <rootzero17@gmail.com>
2022-06-28 20:29:42 +0900

    style setting2

--- src/MobileGlobalStyle.jsx
+++ src/MobileGlobalStyle.jsx
@@ -5,11 +5,6 @@ const GlobalStyle = createGlobalStyle `
     margin: 0;
     padding: 0;
 }
-body {
-    width: 100vw;
-    height: 100vh;
-    background-color: #ddd;
-}
 `;
 
 export default GlobalStyle;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,36 @@
-import React from 'react'
+import styled from "styled-components"
+import welcomeScreen from "../images/home/WelcomeScreen.png";
+import Logo from "../images/home/Logo.png";
 
 const Home = () => {
     
   return (
-    <div>
-        <div style={{backgroundColor:"white"}}>Home</div>
-    </div>
+    <StDiv>
+        <StImg src={welcomeScreen}/>
+        <StLogo src={Logo}/>
+    </StDiv>
   )
 }
+
+const StDiv = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
+const StImg = styled.img`
+  width: 100%;
+  object-fit: cover;
+`;
+
+const StLogo = styled.img`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 60%;
+  height: 8%;
+  object-fit: cover;
+`;
+
 
 export default Home;
